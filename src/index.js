362591e047a5c9bc483decc13495cb0154dbd159
青曳 <zhentao.lzt@alibaba-inc.com>
2018-05-20 19:49:51 +0800

    update readme

--- src/index.js
+++ src/index.js
@@ -10,17 +10,17 @@ puppeteer.launch({ headless: false }).then(async (browser) => {
     /**
      * 搜索引擎
      */
-    await require('./data-source/so.js')(browser, timeout, key)
-    await require('./data-source/sogou.js')(browser, timeout, key)
-    await require('./data-source/baidu.js')(browser, timeout, key)
-    await require('./data-source/bing.js')(browser, timeout, key)
+    // await require('./data-source/so.js')(browser, timeout, key)
+    // await require('./data-source/sogou.js')(browser, timeout, key)
+    // await require('./data-source/baidu.js')(browser, timeout, key)
+    // await require('./data-source/bing.js')(browser, timeout, key)
     
     /**
      * 其他
      */
-    // await require('./data-source/tianyancha.js')(browser, timeout, company)
+    await require('./data-source/tianyancha.js')(browser, timeout, company)
   } catch (error) {
-    // console.log('endTime:', new Date().toUTCString());
+    console.log('endTime:', new Date().toUTCString());
   }
-  // console.log('endTime:', new Date().toUTCString());
+  console.log('endTime:', new Date().toUTCString());
 });
